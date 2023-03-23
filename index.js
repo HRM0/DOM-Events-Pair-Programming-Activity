2f0cbcd4c3403c21f7639ae229d5b18d7be4ec4d
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-03-23 11:14:31 -0400

    cleaned up the look a bit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let ptxt = document.querySelector(".ptext");
 let textsize = 18
 
 bigger.addEventListener("click", function() {
-    
+
     if (textsize === 100) {
         console.log("can't be bigger than 100")
     } else {
@@ -39,59 +39,48 @@ smaller.addEventListener("click", function() {
 
 //problem 3
 const bgcolor = document.querySelector(".changeColor");
+let color = document.querySelector(".input");
 
 bgcolor.addEventListener("click", function() {
-
-    let color = document.querySelector(".input");
     document.body.style.backgroundColor= color.value;
 })
 
 //problem 4
 const up_bot = document.querySelector(".up");
-
+const down_bot = document.querySelector(".down");
+const left_bot = document.querySelector(".left");
+const right_bot = document.querySelector(".right");
+let crane_pic = document.querySelector(".crane");
 let up_move = 0;
+let down_move = 0;
+let left_move = 0;
+let right_move = 0;
 
 up_bot.addEventListener("click", function() {
     up_move -= 1
-    let crane_pic = document.querySelector(".crane");
     crane_pic.style.top = `${up_move}px`;
 })
 
-const down_bot = document.querySelector(".down");
-
-let down_move = 0;
-
 down_bot.addEventListener("click", function() {
     down_move += 1
-    let crane_pic = document.querySelector(".crane");
     crane_pic.style.top = `${down_move}px`;
 })
 
-const left_bot = document.querySelector(".left");
-
-let left_move = 0;
-
 left_bot.addEventListener("click", function() {
     left_move -= 1
-    let crane_pic = document.querySelector(".crane");
     crane_pic.style.left = `${left_move}px`;
 })
 
-const right_bot = document.querySelector(".right");
-
-let right_move = 0;
-
 right_bot.addEventListener("click", function() {
     right_move += 1
-    let crane_pic = document.querySelector(".crane");
     crane_pic.style.left = `${right_move}px`;
 })
 
 //problem 5
 const roll_bot = document.querySelector(".roll");
+let roll_res = document.querySelector(".roll_result");
+let dice_side = document.querySelector(".dice_input");
 
 roll_bot.addEventListener("click", function() {
-    let roll_res = document.querySelector(".roll_result");
-    let dice_side = document.querySelector(".dice_input");
     roll_res.textContent= Math.ceil(dice_side.value * Math.random());
 })
